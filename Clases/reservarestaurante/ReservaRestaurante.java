cec379320cabf3d37b0cc37bec953692acbe8ba4
Lucasalves321 <lucas.alves.pereiralima@gmail.com>
2024-04-28 04:07:24 -0300

    mudou quse nd

--- Clases/reservarestaurante/ReservaRestaurante.java
+++ Clases/reservarestaurante/ReservaRestaurante.java
@@ -13,39 +13,39 @@ public class ReservaRestaurante {
         System.out.println("Seja Bem Vindo");
         int opcao = 0;
         do {
-            System.out.println("1. Fazer Uma Reserva");
-            System.out.println("2. Consultar Reserva");
-            System.out.println("3. Fazer pedido");
-            System.out.println("4. cadastrar Produtos ");
-            System.out.println("5. cadastrar Funcionarios");
-            System.out.println("6. cadastrar mesa");
-            System.out.println("7. cadastar cardapio");
-            System.out.println("8. fechar conta/comanda");
-            System.out.println("9. gerar relatorio");
-            System.out.println("0. Para sair");
+            System.out.println("1.Fazer Uma Reserva");
+            System.out.println("2.Consultar Reserva");
+            System.out.println("3.Fazer pedido");
+            System.out.println("4.Cadastrar Produtos ");
+            System.out.println("5.Cadastrar Funcionarios");
+            System.out.println("6.Cadastrar mesa");
+            System.out.println("7.Cadastar cardapio");
+            System.out.println("8.Fechar conta/comanda");
+            System.out.println("9.Gerar relatorio");
+            System.out.println("0.Para sair");
             opcao = read.nextInt();
 
             switch (opcao) {
                 case 1:
-                    
+
                     System.out.println("Infome o nome do Cliente");
                     read.nextLine();
                     String nomecli = read.nextLine();
                     cli.setnome(nomecli);
-                    
+
                     System.out.println("Infome o CPF");
                     double cpf = read.nextDouble();
                     cli.setCPF(cpf);
-                    
+
                     System.out.println("Informe o numero de Telefone");
                     double tel = read.nextDouble();
                     cli.setTelefone(tel);
-                    
+
                     System.out.println("infome o Email para contato");
                     read.nextLine();
                     String email = read.nextLine();
-                    cli.setEmail(email); 
-                    
+                    cli.setEmail(email);
+
                     System.out.println("Informe o numero da mesa");
                     int numeroMesa = read.nextInt();
                     ms.setmesa(numeroMesa);
@@ -54,6 +54,23 @@ public class ReservaRestaurante {
 
                     break;
                 case 3:
+                    System.out.println("O que deseja pedir?");
+                    System.out.println("1. Comida");
+                    System.out.println("2. Bebida");
+                    System.out.println("3. Sobremesa");
+                    int opcao2 = read.nextInt();
+                    switch (opcao2){
+                        case 1:
+                            System.out.println("Comida escolhida com sucesso");
+                            break;
+                        case 2:
+                            System.out.println("Bebida escolhida com sucesso");
+                            break;
+                        case 3:
+                            System.out.println("Sobremesa escolhida com sucesso");
+                            break;
+                        default:
+                    }
                     break;
                 case 4:
                     break;
